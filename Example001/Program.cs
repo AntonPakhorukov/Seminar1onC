e3a3fccddf4fd5f399c718432b2bfd063312226c
Anton Pakhorukov <pakhorukov.a@mail.ru>
2022-09-16 01:17:55 +0700

    Первые три задачи

---
+++ Example001/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int b = a * a;
+Console.WriteLine("Квадрат числа равен: "+ b);
+
+/* 
+//Напишите программу, которая на вход принимает число и выдаёт его квадрат 
+//(число умноженное на само себя).
+//4 -> 16 
+//-3 -> 9 
+//-7 -> 49
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Квадрата числа " + number +  " равен " + number * number);
+
+Console.WriteLine("Квадрата числа " + number +  " равен " + Math.Pow(number,2));
+*/
