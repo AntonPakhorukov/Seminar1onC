c54a156a9c1e6a973314de8ca06d82dce66ed287
Anton Pakhorukov <pakhorukov.a@mail.ru>
2022-09-16 02:26:14 +0700

    Seminar 1

---
+++ Example004/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая на вход принимает одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+Console.Clear();
+
+Console.Write("ВВедите число: ");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = -number1;
+//Console.WriteLine(number1);
+//Console.WriteLine(number2);
+while (number1 != number2) {
+    Console.Write(number1);
+    number1 = number1 - 1;
+}
---
+++ Example005/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Напишите программу, которая принимает на вход трёхзначное число и на 
+выходе показывает последнюю цифру этого числа.
+	456 -> 6
+	782 -> 2
+	918 -> 8
+*/
+
+Console.Clear();
+Console.Write("Введите трех значное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number / 100 < 1) {
+    Console.WriteLine("Вы ввели не трех значное число");
+    return;
+} else if (number / 100 > 10) {
+    Console.WriteLine("Вы ввели не трех значное число");
+    return;
+}
+Console.WriteLine(number % 10);
